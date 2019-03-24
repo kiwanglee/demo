12714e6ff1a6de02a53598152e3c4b1107b70d0c
kiwang <kiwang.lee@kurlycorp.com>
2019-03-24 17:45:17 +0900

    oAuth 적용중..

--- src/main/java/com/example/demo/config/security/SecurityConfig.java
+++ src/main/java/com/example/demo/config/security/SecurityConfig.java
@@ -2,6 +2,8 @@ package com.example.demo.config.security;
 
 import com.example.demo.service.LoginService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.security.oauth2.client.OAuth2ClientProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -9,11 +11,17 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-
+// 14e16010bd910c8a817970582ae9bbea kakao apikey
     @Autowired
     private LoginService loginService;
 
@@ -24,8 +32,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/css/**", "/index").permitAll()
+                .antMatchers("/css/**", "/index", "/oauth2/**").permitAll()
                 .antMatchers("/user/**").access("hasAuthority('USER')")
+                .and()
+                .oauth2Login().defaultSuccessUrl("/user").failureUrl("/login-error")
                 .and()
                 .formLogin().usernameParameter("user").passwordParameter("password")
                 .loginPage("/login").failureUrl("/login-error");
@@ -41,5 +51,29 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     public PasswordEncoder passwordEncoder() {
 
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public ClientRegistrationRepository clientRegistrationRepostiory(OAuth2ClientProperties oAuth2ClientProperties,
+                                                                     @Value("${custom.oauth2.kakao.client-id}") String kakaoClientId) {
+
+        List<ClientRegistration> registrations = oAuth2ClientProperties
+                .getRegistration().keySet().stream()
+                .map(client -> getRegistration(oAuth2ClientProperties, client))
+                .filter(Objects::nonNull)
+                .collect(Collectors.toList());
+
+        registrations.add(CustomOAuth2Provider.KAKAO.getBuilder("kakao")
+                .clientId(kakaoClientId)
+                .clientSecret("test")
+                .jwkSetUri("test")
+                .build());
+
+        return null;
+    }
+
+    private ClientRegistration getRegistration(OAuth2ClientProperties oAuth2ClientProperties, String client) {
+
+        return null;
     }
 }
